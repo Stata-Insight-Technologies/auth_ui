5491ba88a1025ff51e177face9c67921cb1b1661
Bello Yakub <hyaaquub@gmail.com>
2022-03-01 22:14:01 +0100

    Feat[User controller]: Add login page

--- app/src/main/java/com/example/waterdelivery/WelcomeFragment.kt
+++ app/src/main/java/com/example/waterdelivery/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.waterdelivery.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -16,6 +17,19 @@ class WelcomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_welcome, container, false).run {
             mBinding = FragmentWelcomeBinding.bind(this)
             this
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mBinding.apply {
+            signUp.setOnClickListener {
+                findNavController().navigate(R.id.action_welcomeFragment_to_signUpFragment)
+            }
+
+            login.setOnClickListener {
+                findNavController().navigate(R.id.action_welcomeFragment_to_loginFragment)
+            }
         }
     }
 
